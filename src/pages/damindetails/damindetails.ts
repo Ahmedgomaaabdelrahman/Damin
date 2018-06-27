960a06901c7c296e7220cd43e64084fe3b0ccce1
midorock2009 <midorock2009@gmail.com>
2018-06-27 22:24:52 +0200

    part 2 of back end

--- src/pages/damindetails/damindetails.ts
+++ src/pages/damindetails/damindetails.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Http} from "@angular/http";
+import {SharedDataProvider} from "../../providers/shared-data/shared-data";
+import {ConfigProvider} from "../../providers/config/config";
 
 /**
  * Generated class for the DamindetailsPage page.
@@ -14,11 +17,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'damindetails.html',
 })
 export class DamindetailsPage {
+  public formData: { [k: string]: any } = {};
+  bankData: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http,
+              public shared: SharedDataProvider,
+              public config: ConfigProvider) {
   }
-
   ionViewDidLoad() {
+    this.formData.customers_id = this.shared.customerData.customers_id;
+    console.log(this.formData)
+    this.shared.show()
+    this.http.post(this.config.url + 'api/getBanks', this.formData).map(res => res.json()).subscribe(data => {
+      this.shared.hide();
+      this.bankData = data.data
+      console.log(data);
+    }, error1 => {
+      console.log(error1)
+    });
     console.log('ionViewDidLoad DamindetailsPage');
   }
 
